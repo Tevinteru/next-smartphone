feac89b24f8529366e3127d61d60e575fa7e583d
Tevinteru <alecksey1231@mail.ru>
2025-06-03 20:09:16 +0800

    fixed mkdir logs

--- shared/lib/logger.ts
+++ shared/lib/logger.ts
@@ -1,7 +1,13 @@
 import winston from 'winston';
 import path from 'path';
+import fs from 'fs';
 
 const logDir = 'logs';
+const isProd = process.env.NODE_ENV === 'production';
+
+if (!isProd && !fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir);
+}
 
 const logger = winston.createLogger({
   level: 'info',
@@ -12,13 +18,18 @@ const logger = winston.createLogger({
     })
   ),
   transports: [
-    new winston.transports.File({
-      filename: path.join(logDir, 'error.log'),
-      level: 'error',
-    }),
-    new winston.transports.File({
-      filename: path.join(logDir, 'combined.log'),
-    }),
+    new winston.transports.Console(),
+    ...(!isProd
+      ? [
+          new winston.transports.File({
+            filename: path.join(logDir, 'error.log'),
+            level: 'error',
+          }),
+          new winston.transports.File({
+            filename: path.join(logDir, 'combined.log'),
+          }),
+        ]
+      : []),
   ],
 });
 
